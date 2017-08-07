dd45d5ce798092ae1db7d54719f88c62339a12b6
bradleyden <bleyden92@gmail.com>
2017-08-07 13:49:19 -0400

    Add orders resource
    Adds a resource to the back end for orders along with curl scripts.
    
    Orders expects a date_placed, isOpen boolean, and a products array.
    When a new order is created, the date_placed will be null, isOpen will
    be true, and products will be empty.
    
    As products are added, the products array will fill up. When an
    order is checked out, isOpen will be set to false and date_placed
    will be set to the current date at the time the order was placed.

---
+++ app/models/order.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+  date_placed: {
+    type: Date,
+    required: true
+  },
+  products: {
+    type: [[mongoose.Schema.Types.ProductId, Number]]
+  },
+  isOpen: {
+    type: Boolean,
+    required: true
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: false,
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
+})
+
+const Order = mongoose.model('Order', orderSchema)
+
+module.exports = Order
