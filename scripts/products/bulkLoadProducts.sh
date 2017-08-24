e76040469dd46da930433c756249381e2f1a8c4d
ryan <ryanwkoch@gmail.com>
2017-08-24 17:13:39 -0400

    Update bulkload scripts with echo
    Added echo to bulk load scripts and added a second variation for more
    testing.

--- scripts/products/bulkLoadProducts.sh
+++ scripts/products/bulkLoadProducts.sh
@@ -1,1 +1,1 @@
-mongoimport --db=heroku_t3qddf72 --collection=products --type=csv --headerline --file=data/products.csv --drop
+echo `mongoimport --db=heroku_t3qddf72 --collection=products --type=csv --headerline --file=data/products.csv --drop`
